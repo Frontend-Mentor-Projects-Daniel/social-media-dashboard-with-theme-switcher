5c931d463334cb6f41845fceeef0fd93f93e0a9d
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-11-02 17:16:15 -0400

    get colour theme to work on firefox and other browsers that don't support has

--- develop/js/script.js
+++ develop/js/script.js
@@ -7,22 +7,23 @@ var storeTheme = function (theme) {
 var setTheme = function () {
     var activeTheme = localStorage.getItem('theme');
     if (typeof activeTheme === 'string') {
+        // check matching radio button
         colorThemes.forEach(function (themeOption) {
             if (themeOption.id === activeTheme) {
                 themeOption.checked = true;
+                // apply theme for firefox and other browsers that don't support :has()
+                document.body.className = themeOption.id;
             }
         });
-        // fallback for no :has() support
-        document.documentElement.className = activeTheme;
     }
 };
 // apply theme
 colorThemes.forEach(function (themeOption) {
     themeOption.addEventListener('click', function () {
         storeTheme(themeOption.id);
+        // apply theme for firefox and other browsers that don't support :has()
+        document.body.className = themeOption.id;
     });
 });
 // apply saved them
-window.addEventListener('DOMContentLoaded', function () {
-    setTheme();
-});
+window.addEventListener('DOMContentLoaded', setTheme);
--- develop/js/script.ts
+++ develop/js/script.ts
@@ -12,14 +12,15 @@ const setTheme = (): void => {
   const activeTheme: string | null = localStorage.getItem('theme');
 
   if (typeof activeTheme === 'string') {
+    // check matching radio button
     colorThemes.forEach((themeOption) => {
       if (themeOption.id === activeTheme) {
         themeOption.checked = true;
+
+        // apply theme for firefox and other browsers that don't support :has()
+        document.body.className = themeOption.id;
       }
     });
-
-    // fallback for no :has() support
-    document.documentElement.className = activeTheme;
   }
 };
 
@@ -27,10 +28,11 @@ const setTheme = (): void => {
 colorThemes.forEach((themeOption) => {
   themeOption.addEventListener('click', () => {
     storeTheme(themeOption.id);
+
+    // apply theme for firefox and other browsers that don't support :has()
+    document.body.className = themeOption.id;
   });
 });
 
 // apply saved them
-window.addEventListener('DOMContentLoaded', () => {
-  setTheme();
-});
+window.addEventListener('DOMContentLoaded', setTheme);
